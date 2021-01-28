b8ce9ca7a85edd2786c5c740f04b04db85198b91
Piyush Arora <apiyush14yahoo.com>
2021-01-28 23:43:45 +0530

    New User flag

--- codesrc/src/main/java/com/onehealth/processors/GetOTPForMSISDNRequestProcessor.java
+++ codesrc/src/main/java/com/onehealth/processors/GetOTPForMSISDNRequestProcessor.java
@@ -8,16 +8,18 @@ import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Example;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import com.onehealth.core.processor.RequestProcessor;
 import com.onehealth.entities.UserAuthenticationDetails;
 import com.onehealth.entities.UserDetails;
 import com.onehealth.model.request.GetOTPForMSISDNRequest;
+import com.onehealth.model.response.GetOTPForMSISDNResponse;
 import com.onehealth.repo.UserAuthenticationDetailsRepository;
 import com.onehealth.repo.UserDetailsRepository;
 
 @Component
-public class GetOTPForMSISDNRequestProcessor extends RequestProcessor<GetOTPForMSISDNRequest, Boolean> {
+public class GetOTPForMSISDNRequestProcessor extends RequestProcessor<GetOTPForMSISDNRequest, GetOTPForMSISDNResponse> {
 
 	@Autowired
 	UserAuthenticationDetailsRepository userAuthenticationDetailsRepo;
@@ -26,13 +28,14 @@ public class GetOTPForMSISDNRequestProcessor extends RequestProcessor<GetOTPForM
 	UserDetailsRepository userDetailsRepo;
 
 	@Override
-	public Boolean doProcessing(GetOTPForMSISDNRequest request) throws Exception {
-		generateOTPForMSISDN(request);
-		return true;
+	public GetOTPForMSISDNResponse doProcessing(GetOTPForMSISDNRequest request) throws Exception {
+		return generateOTPForMSISDN(request);
 	}
 
 	// To be replaced with the real call to SMS Gateway Service
-	private void generateOTPForMSISDN(GetOTPForMSISDNRequest request) {
+	private GetOTPForMSISDNResponse generateOTPForMSISDN(GetOTPForMSISDNRequest request) {
+		
+		GetOTPForMSISDNResponse response=new GetOTPForMSISDNResponse();
 		int otpCode = (int) Math.abs(((Math.random() * (0001 - 9999)) + 0001));
 		
 		UserAuthenticationDetails userAuthenticationDetailsQueryObj = new UserAuthenticationDetails();
@@ -44,6 +47,9 @@ public class GetOTPForMSISDNRequestProcessor extends RequestProcessor<GetOTPForM
 		
 		if(userAuthenticationDetailsOptional.isPresent()) {
 			UserAuthenticationDetails userAuthenticationDetails=userAuthenticationDetailsOptional.get();
+			if(StringUtils.isEmpty(userAuthenticationDetails.getUserSecretKey())) {
+				response.setNewUser(true);
+			}
 			userAuthenticationDetails.setUserGeneratedOTP(String.valueOf(otpCode));
 			userAuthenticationDetailsRepo.save(userAuthenticationDetails);
 		}
@@ -56,8 +62,11 @@ public class GetOTPForMSISDNRequestProcessor extends RequestProcessor<GetOTPForM
 			userAuthenticationDetails.setUserGeneratedOTP(String.valueOf(otpCode));
 			userAuthenticationDetails.setUserDetails(userDetails);
 			userAuthenticationDetailsRepo.save(userAuthenticationDetails);
+			response.setNewUser(true);
 		}
 		System.out.println("===========Generated OTP For MSISDN===================" + otpCode);
+		response.setStatus(true);
+		return response;
 	}
 
 	@Transactional
